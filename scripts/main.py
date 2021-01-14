b5b214f995d6f2f0e3a617e2f1c4829d7c383d60
Michael F. Adamer <michael.adamer@bsse.ethz.ch>
2021-01-14 08:39:36 +0100

    added a linear AE (PCA)

--- scripts/main.py
+++ scripts/main.py
@@ -2,7 +2,7 @@ import sys
 import torch
 import pytorch_lightning as pl
 import numpy as np
-from src.models.mag_ae import MagAE
+from src.models.mag_ae import MagAE, PCAE
 from tests.synthetic_data import Spheres
 from torch.utils.data import TensorDataset, DataLoader, random_split
 
@@ -10,7 +10,7 @@ from torch.utils.data import TensorDataset, DataLoader, random_split
 
 def main():
     spheres = Spheres()
-    data = spheres.generate()
+    data = spheres.generate(1000)
 
     # train_size = int(np.ceil(0.8*data.shape[0]))
     # val_size = int(data.shape[0]-np.ceil(0.8*data.shape[0]))
@@ -18,16 +18,17 @@ def main():
 
     data_loader = DataLoader(data,batch_size=32,shuffle=True,num_workers=8)
 
-    model = MagAE(101)
+    model = PCAE(101)
     trainer = pl.Trainer(max_epochs=4)
     trainer.fit(model,data_loader)
 
 # Test the trained model as I don't at all trust it.
     import matplotlib.pyplot as plt
 
-    label = np.hstack([i*np.ones(1000) for i in range(11)])
+    val_points = 1000
+    label = np.hstack([i*np.ones(val_points) for i in range(11)])
 
-    low_dim = model.forward(spheres.generate_more()).detach().numpy()
+    low_dim = model.forward(spheres.generate(val_points)).detach().numpy()
     plt.scatter(low_dim[:,0],low_dim[:,1],c=label,cmap='twilight')
     plt.show()
 
--- src/models/mag_ae.py
+++ src/models/mag_ae.py
@@ -33,7 +33,29 @@ class MagAE(pl.LightningModule):
         x = batch
         h = self.encoder(x)
         x_hat = self.decoder(h)
-        loss = F.mse_loss(x,x_hat,reduction='sum')
+        loss = F.mse_loss(x,x_hat,reduction='mean')
+        self.log('train_loss',loss)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(),lr=1e-3,weight_decay=1e-5)
+        return optimizer
+
+class PCAE(pl.LightningModule):
+    def __init__(self,input_size):
+        super(PCAE,self).__init__()
+        self.input_size = input_size
+        self.encoder = nn.Linear(self.input_size,2)
+        self.decoder = nn.Linear(2,self.input_size)
+    def forward(self, x):
+        h = self.encoder(x)
+        return h
+
+    def training_step(self,batch,batch_idx):
+        x = batch
+        h = self.encoder(x)
+        x_hat = self.decoder(h)
+        loss = F.mse_loss(x,x_hat,reduction='mean')
         self.log('train_loss',loss)
         return loss
 
