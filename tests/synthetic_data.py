dfdef1f59e9f366495c688a73783f9db3f7f3e84
Michael F. Adamer <michael.adamer@bsse.ethz.ch>
2021-01-14 08:38:41 +0100

    restructured

--- tests/synthetic_data.py
+++ tests/synthetic_data.py
@@ -4,36 +4,33 @@ from torch.utils.data import TensorDataset
 from tadasets import dsphere
 
 class Spheres():
-    def __init__(self,d=100,n_spheres=10,r=5,datapoints=1000,tensor=True):
+    def __init__(self,d=100,n_spheres=10,r=5,tensor=True):
         self.d = d
         self.n_spheres = n_spheres
         self.r =r
-        self.datapoints = datapoints
         self.tensor = tensor
-        self.shifts = np.zeros([self.n_spheres,self.d+1])
+        self.shifts = np.random.randn(self.n_spheres,self.d+1)
 
-    def generate(self):
+    def generate(self,datapoints):
         if self.tensor:
-            return torch.as_tensor(self._generate_sphere_data(),dtype=torch.float)
+            return torch.as_tensor(self._generate_sphere_data(datapoints),dtype=torch.float)
         else:
-            return self._generate_sphere_data()
+            return self._generate_sphere_data(datapoints)
 
-    def generate_more(self):
-        if self.tensor:
-            return torch.as_tensor(self._generate_sphere_data(more=True),dtype=torch.float)
-        else:
-            return self._generate_sphere_data(more=True)
 
-    def _generate_sphere_data(self,more=False):
-        if not more:
-            self.shifts = np.random.randn(self.n_spheres,self.d+1)
+    def _generate_sphere_data(self,datapoints):
         sigma = np.sqrt(10.0/np.sqrt(float(self.d+1)))*np.eye(self.d+1)
         shifts = [np.matmul(sigma,self.shifts[i].reshape(-1,1)).reshape(1,-1) for i in range(self.n_spheres)]
-        spheres = [shifts[i] + dsphere(n=self.datapoints,d=self.d,r=self.r) for i in range(self.n_spheres)]
-        envelope = dsphere(n=self.datapoints,d=self.d,r=5*self.r)
+        spheres = [shifts[i] + dsphere(n=datapoints,d=self.d,r=self.r) for i in range(self.n_spheres)]
+        envelope = dsphere(n=datapoints,d=self.d,r=5*self.r)
         spheres.append(envelope)
         return np.vstack(spheres)
 
+    # def generate_more(self):
+    #     if self.tensor:
+    #         return torch.as_tensor(self._generate_sphere_data(more=True),dtype=torch.float)
+    #     else:
+    #         return self._generate_sphere_data(more=True)
 
 # def generate_sphere_data(d,n_spheres,r):
 #     shifts = np.random.randn(n_spheres,d+1)
